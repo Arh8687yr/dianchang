9ae69ea285e3e451c1201c1c27fa734f4777d205
执念 <1250696730@qq.com>
2020-04-02 14:26:48 +0800

    震动分析-数据列表显示

--- src/views/overall/fault/structrue.vue
+++ src/views/overall/fault/structrue.vue
@@ -1,41 +1,23 @@
 <template>
-    <basic-container>
-        <Heads />
-            <el-main>
-                <el-table
-                    :data="tableData"
-                    height="250"
-                    border
-                    style="width: 100%">
-                    <el-table-column
-                    align="center"
-                    prop="date"
-                    label="序号"
-                    width="150">
-                    </el-table-column>
-                    <el-table-column
-                    align="center"
-                    prop="name"
-                    label="通道名称"
-                    width="280">
-                    </el-table-column>
-                    <el-table-column
-                    prop="address"
-                    align="center"
-                    label="诊断结果">
-                    </el-table-column>
-                </el-table>
-                     <el-pagination
-                        @size-change="handleSizeChange"
-                        @current-change="handleCurrentChange"
-                        :current-page.sync="currentPage2"
-                        :page-sizes="[5]"
-                        :page-size="5"
-                        :total="100">
-                        </el-pagination>
-            </el-main>
-            <Place />
-    </basic-container>
+  <basic-container>
+    <Heads />
+    <el-main>
+      <el-table :data="tableData" height="250" border style="width: 100%">
+        <el-table-column align="center" prop="date" label="序号" width="150"></el-table-column>
+        <el-table-column align="center" prop="name" label="通道名称" width="280"></el-table-column>
+        <el-table-column prop="address" align="center" label="诊断结果"></el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage2"
+        :page-sizes="[5]"
+        :page-size="5"
+        :total="100"
+      ></el-pagination>
+    </el-main>
+    <Place />
+  </basic-container>
 </template>
 
 <script>
@@ -51,6 +33,125 @@ import Place from '../../place'
         name : '',
         time : '',
         rotate: '',
+         tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        }
+      ]
       };
     },
       mounted() {
@@ -61,34 +162,34 @@ import Place from '../../place'
 </script>
 
 <style scoped>
-    
-    .el-header, .el-footer {
-        line-height: 50px;
-        font-weight: bold;
-    }
-    .el-main {
-        background-color: #fff;
-        color: #333;
-        font-weight: bold;
-    }
-    .select{
-        margin: 20px 40px 0 20px;
-        color: #000;
-        text-indent: 2px;
-    }
-     .el-header i{
-        font-size: 20px;
-        color: #fff;
-    }
-    .el-main i{
-        font-size: 15px;
-    }
-    .content{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
-    .el-footer{
-        background-color: skyblue;
-    }
+.el-header,
+.el-footer {
+  line-height: 50px;
+  font-weight: bold;
+}
+.el-main {
+  background-color: #fff;
+  color: #333;
+  font-weight: bold;
+}
+.select {
+  margin: 20px 40px 0 20px;
+  color: #000;
+  text-indent: 2px;
+}
+.el-header i {
+  font-size: 20px;
+  color: #fff;
+}
+.el-main i {
+  font-size: 15px;
+}
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.el-footer {
+  background-color: skyblue;
+}
 </style>
--- src/views/overall/raletime/list.vue
+++ src/views/overall/raletime/list.vue
@@ -1,15 +1,208 @@
 <template>
-    <basic-container>
-        <h2>数据列表显示</h2>
-    </basic-container>
+  <basic-container>
+    <heads />
+    <el-main>
+      <div class="box1">
+        <div class="left">
+          选择时间段:
+          <el-select v-model="time" clearable placeholder="最近2分钟">
+            <el-option
+              v-for="time in options1"
+              :key="time.value"
+              :label="time.label"
+              :value="time.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="aside">
+          <el-button
+            style="background-color: #409EFF;color: #fff;margin-left: 30px"
+            icon="el-icon-search"
+          >立即查询</el-button>
+        </div>
+      </div>
+      <div class="box2">
+        <el-table :data="tableData" height="425" border style="width: 100%">
+          <el-table-column align="center" type="index" width="50" label="序号"></el-table-column>
+          <el-table-column align="center" prop="name" label="时间"></el-table-column>
+          <el-table-column align="center" prop="address" label="转速"></el-table-column>
+          <el-table-column align="center" prop="address" label="功率"></el-table-column>
+          <el-table-column align="center" prop="address" label="机组负荷"></el-table-column>
+          <el-table-column align="center" prop="address" label="主蒸汽压力"></el-table-column>
+          <el-table-column align="center" prop="address" label="主蒸汽温度"></el-table-column>
+        </el-table>
+      </div>
+    </el-main>
+    <place />
+  </basic-container>
 </template>
 
 <script>
-    export default{
+import Heads from "../../heads";
+import Place from "../../place";
 
-    }
+export default {
+  components: {
+    Heads,
+    Place
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+          tag: "公司"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+          tag: "家"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+          tag: "公司"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-    
+<style scoped>
+.el-main {
+  background-color: #fff;
+  color: #333;
+  height: 70vh;
+}
+.select {
+  margin: 20px 40px 0 20px;
+  color: #000;
+  text-indent: 2px;
+}
+
+.el-main i {
+  font-size: 15px;
+}
+.box1,
+.box2 {
+  display: flex;
+  /* justify-content: flex-start; */
+  justify-content: space-between;
+  margin: 10px 10px 0 10px;
+  padding: 0;
+}
+.aside {
+  line-height: 117px;
+  margin-left: 20px;
+}
+.el-select {
+  margin-top: 40px;
+}
+.el-scrollbar__wrap {
+  overflow: hidden;
+}
 </style>
